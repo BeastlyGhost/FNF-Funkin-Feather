4764cb568d0aeae74f76ceb217cc8f6f32bab61c
Gabriela <45212377+BeastlyGhost@users.noreply.github.com>
2022-11-27 15:59:40 -0400

    fix gf dance + docs folder for later use

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -1,5 +1,6 @@
 package states;
 
+import base.song.Conductor;
 import base.song.MusicState;
 import flixel.FlxG;
 import flixel.FlxSprite;
@@ -8,13 +9,15 @@ import objects.Alphabet;
 
 typedef TitleIntroText =
 {
-	var bg:String;
-	var bgFolder:String;
-	var bgAntialias:Bool;
-	var gfAntialias:Bool;
+	@:optional var bg:String;
+	@:optional var gf:String;
+	@:optional var bgFolder:String;
+	@:optional var gfFolder:String;
+	@:optional var bgAntialias:Bool;
+	@:optional var gfAntialias:Bool;
 
-	var stepText:IntroLines;
-	var linesRandom:Array<String>; // replaces "introText.txt"
+	@:optional var stepText:IntroLines;
+	@:optional var linesRandom:Array<String>; // replaces "introText.txt"
 }
 
 typedef IntroLines =
@@ -26,14 +29,20 @@ typedef IntroLines =
 
 class TitleState extends MusicBeatState
 {
-	private var introLines:TitleIntroText;
-	private var introTxt:Array<Alphabet>;
+	// temporary, this is gonna be a json on the assets folder later
+	var introLines:TitleIntroText = {
+		gf: "title/gfDanceTitle",
+		gfFolder: "images/menus",
+		bgAntialias: true,
+		gfAntialias: true,
+	};
+	var introTxt:Array<Alphabet>;
 
-	private var started:Bool = false;
-	private var skipped:Bool = false;
+	var started:Bool = false;
+	var skipped:Bool = false;
 
-	private var gfDance:FlxSprite;
-	private var titleEnter:FlxSprite;
+	var gfDance:FlxSprite;
+	var titleEnter:FlxSprite;
 
 	override function create()
 	{
@@ -52,19 +61,23 @@ class TitleState extends MusicBeatState
 
 		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, 0xFF000000);
 		bg.scrollFactor.set();
+		bg.antialiasing = introLines.bgAntialias;
 		add(bg);
 
 		// gf
 		gfDance = new FlxSprite(FlxG.width * 0.4, FlxG.height * 0.07);
-		gfDance.frames = AssetHandler.grabAsset('title/gfDanceTitle', SPARROW, "images/menus");
+		gfDance.frames = AssetHandler.grabAsset(introLines.gf, SPARROW, introLines.gfFolder);
 		gfDance.animation.addByIndices('danceLeft', 'gfDance', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
 		gfDance.animation.addByIndices('danceRight', 'gfDance', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
-		gfDance.antialiasing = true;
+		gfDance.antialiasing = introLines.gfAntialias;
 		add(gfDance);
 	}
 
 	override function update(elapsed:Float)
 	{
+		if (FlxG.sound.music != null)
+			Conductor.songPosition = FlxG.sound.music.time;
+
 		super.update(elapsed);
 
 		if (Controls.getPressEvent("accept"))
