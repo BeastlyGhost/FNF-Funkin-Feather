893beedc0a6270444de3b1bd884298272093ed68
MemeHoovy <97995998+MemeHoovy@users.noreply.github.com>
2022-12-05 16:41:13 -0500

    Update Note.hx (#6)

--- source/funkin/objects/ui/notes/Note.hx
+++ source/funkin/objects/ui/notes/Note.hx
@@ -135,7 +135,7 @@ class Note extends FeatherSprite
 				&& step < Conductor.songPosition + (Conductor.safeZoneOffset * 0.5))
 				canBeHit = true;
 			else
-				canBeHit = true;
+				canBeHit = false;
 		}
 		else
 		{
