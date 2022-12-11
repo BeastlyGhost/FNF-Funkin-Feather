f732cf80eb459aacb82f0046aa633248165a5fb0
Gabriela <45212377+BeastlyGhost@users.noreply.github.com>
2022-12-11 18:44:56 -0400

    formatting

--- source/funkin/objects/ui/DialogueBox.hx
+++ source/funkin/objects/ui/DialogueBox.hx
@@ -5,72 +5,73 @@ import flixel.group.FlxSpriteGroup;
 
 @:enum abstract BoxTextForm(String) to String
 {
-    var BASE = 'base';
-    var PIXEL = 'pixel';
-}
-typedef BoxDataForm =
-{
-    var texture:String;
-    var texturePath:String;
-    var ?textForm:String;
-    var ?position:Array<Float>;
-    var ?animated:Bool;
-    var ?framerate:Float;
-    var ?size:Float;
+	var BASE = 'base';
+	var PIXEL = 'pixel';
 }
 
-typedef CharDataForm = 
+typedef BoxDataForm =
 {
-    var texture:String;
-    var texturePath:String;
-    var animations:Array<CharAnimForm>;
+	var texture:String;
+	var texturePath:String;
+	var ?textForm:String;
+	var ?position:Array<Float>;
+	var ?animated:Bool;
+	var ?framerate:Float;
+	var ?size:Float;
+}
+
+typedef CharDataForm =
+{
+	var texture:String;
+	var texturePath:String;
+	var animations:Array<CharAnimForm>;
 }
 
 typedef CharAnimForm =
 {
-    var anim:Null<String>;
+	var anim:Null<String>;
 	var animOffset:Array<Float>;
-    var animDefault:Null<String>;
+	var animDefault:Null<String>;
 }
 
 class DialogueBox extends FlxSpriteGroup
 {
-    public var boxData:BoxDataForm;
-    public var charData:CharDataForm;
+	public var boxData:BoxDataForm;
+	public var charData:CharDataForm;
 
-    public var textList:Array<String> = [];
+	public var textList:Array<String> = [];
 
-    override public function new(file:String):Void
-    {
-        // dialogue box defaults
-        boxData = {
-            texture: "speech_bubble_talking",
-            texturePath: "images/dialogue",
-            animated: true,
-            textForm: BASE,
-            position: [0, 0],
-            framerate: 24,
-            size: 1
-        };
+	override public function new(file:String):Void
+	{
+		// dialogue box defaults
+		boxData = {
+			texture: "speech_bubble_talking",
+			texturePath: "images/dialogue",
+			animated: true,
+			textForm: BASE,
+			position: [0, 0],
+			framerate: 24,
+			size: 1
+		};
 
-        initBox(boxData.texture, file);
+		initBox(boxData.texture, file);
 
-        super();
-    }
+		super();
+	}
 
-    public function initBox(tex:String, file:String):FlxSprite
-    {
-        var type:AssetType = (boxData.animated ? SPARROW : IMAGE);
+	public function initBox(tex:String, file:String):FlxSprite
+	{
+		var type:AssetType = (boxData.animated ? SPARROW : IMAGE);
 
-        var diagBox:FlxSprite = new FlxSprite();
-        if (type == SPARROW)
-            diagBox.frames = AssetHandler.grabAsset(tex, SPARROW, boxData.texturePath)
-        else
-            diagBox.loadGraphic(AssetHandler.grabAsset(tex, IMAGE, boxData.texturePath));
+		var diagBox:FlxSprite = new FlxSprite();
+		if (type == SPARROW)
+			diagBox.frames = AssetHandler.grabAsset(tex, SPARROW, boxData.texturePath)
+		else
+			diagBox.loadGraphic(AssetHandler.grabAsset(tex, IMAGE, boxData.texturePath));
 
-        diagBox.screenCenter();
-        diagBox.setPosition(boxData.position[0], boxData.position[1]);
+		diagBox.screenCenter();
+		diagBox.setPosition(boxData.position[0], boxData.position[1]);
 
-        return diagBox;
-    }
+		return diagBox;
+	}
 }
