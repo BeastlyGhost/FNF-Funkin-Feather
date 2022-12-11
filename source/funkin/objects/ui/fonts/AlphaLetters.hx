6e3524cbc8f4fa54454ab3de13aa54ce19f346d5
doggogit <andrejjosifbt@gmail.com>
2022-12-11 01:31:59 +0100

    formatting

--- source/funkin/objects/ui/fonts/AlphaLetters.hx
+++ source/funkin/objects/ui/fonts/AlphaLetters.hx
@@ -10,10 +10,8 @@ typedef Letter =
 class AlphaLetters
 {
 	public static var letterMap:Map<String, Null<Letter>> = [
-		"a" => null, "b" => null, "c" => null, "d" => null, "e" => null,
-		"f" => null, "g" => null, "h" => null, "i" => null, "j" => null,
-		"k" => null, "l" => null, "m" => null, "n" => null, "o" => null,
-		"p" => null, "q" => null, "r" => null, "s" => null, "t" => null,
-		"u" => null, "v" => null, "w" => null, "x" => null, "y" => null,
-		"z" => null];
+		"a" => null, "b" => null, "c" => null, "d" => null, "e" => null, "f" => null, "g" => null, "h" => null, "i" => null, "j" => null, "k" => null,
+		"l" => null, "m" => null, "n" => null, "o" => null, "p" => null, "q" => null, "r" => null, "s" => null, "t" => null, "u" => null, "v" => null,
+		"w" => null, "x" => null, "y" => null, "z" => null
+	];
 }
